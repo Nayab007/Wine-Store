8c546f98838835e8f15b36aff68bed5efec18fed
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2021-12-18 19:11:00 +0100

    add

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,7 +23,7 @@ export const services = [
     id: 1,
     icon: <GiCompass />,
     title: 'mission',
-    text: 'Lorem ipsum, dolor sit amet connsecttetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+    text: 'Lorem ipsum, dolor sit amet consecttetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
   },
   {
     id: 2,

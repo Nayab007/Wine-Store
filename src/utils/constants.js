3ce8fdddb5515c3b42ecc1b225aa2b92b5d3f2a1
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2021-12-20 01:01:58 +0100

    add

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -29,7 +29,7 @@ export const services = [
     id: 2,
     icon: <GiDiamondHard />,
     title: 'vision',
-    text: 'Lorem ispuum, dolor sit amet consectetur addipisicing elit. Vooluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+    text: 'Lorem ispuum, dolor sit amet consectetur aaddipisicing elit. Vooluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
   },
   {
     id: 3,

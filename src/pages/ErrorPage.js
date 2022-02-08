4b18d8cc7152fa3f6cc837adaac33aded70b75da
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-02-09 00:13:21 +0100

    add

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -8,7 +8,7 @@ const ErrorPage = () => {
       <h1>404</h1>
       <h3>Sorry, the page cannot be found</h3>
       <Link to="/" className="btn">
-        back to Home
+        back to Hme
       </Link>
     </section>
     

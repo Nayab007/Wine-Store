861417220eb5a3e04ab92965fc5cc38cd209f980
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-06-06 00:11:40 +0200

    add

--- src/components/Product.js
+++ src/components/Product.js
@@ -36,7 +36,7 @@ const Wrapper = styled.article`
     position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-55%, -50%);
+    transform: translate(-54%, -50%);
     background: var(--clr-primary-5);
     display: flex;
     align-items: center;
